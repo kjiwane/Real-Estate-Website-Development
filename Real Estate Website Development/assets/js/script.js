e9b9934f2857640e63c8fbe8ed6123b2bbfbad25
kjiwane <126439947+kjiwane@users.noreply.github.com>
2024-12-28 05:30:27 +0530

    Add files via upload

---
+++ Real Estate Website Development/assets/js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** navbar toggle in mobile */
+
+const /**{NodeElement} */ $navbar = document.querySelector("[data-navbar]");
+const /**{NodeElement} */ $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click",() => $navbar.classList.toggle("active"));
+
+/** heager scroll state */
+
+const /**{NodeElement} */ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", e =>{
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+
+/** add to favorite button toggle */
+
+const /**{NodeList} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    });
+});
